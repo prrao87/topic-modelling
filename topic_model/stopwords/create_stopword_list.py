5dbf3918a3cc4b217a167c4a4c327acda92f011e
prrao87 <prrao87@gmail.com>
2020-01-24 21:00:03 -0800

    Add code for creating custom stopword list

---
+++ topic_model/stopwords/create_stopword_list.py
@@ -0,0 +1,25 @@
+"""
+Script to generate a custom list of stopwords that extend upon existing lists.
+"""
+import spacy
+
+# In this example, we first get the default spaCy stopword list
+nlp = spacy.blank('en')
+spacy_stopwords = nlp.Defaults.stop_words
+print(f"Obtained {len(list(spacy_stopwords))} from spaCy.")
+
+# These words are very common and do not add value to topic models
+add_stopwords = set(
+    ['mr', 'mrs', 'ms', 'dr', 'mr.', 'mrs.', 'ms.', 'dr.',
+     'man', 'woman', 'de'
+     ]
+)
+
+combined_stopwords = spacy_stopwords.union(add_stopwords)
+stopword_list = list(combined_stopwords)
+print(f"New stopword list contains {len(stopword_list)} words.")
+
+# Write out stopwords to file
+with open('stopwords/custom_stopwords.txt', 'w') as f:
+    for word in stopword_list:
+        f.write(word + '\n')
