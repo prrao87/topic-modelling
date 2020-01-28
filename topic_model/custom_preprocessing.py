9cf290a89023878adc029751549185b1287bb49d
prrao87 <prrao87@gmail.com>
2020-01-27 18:01:02 -0800

    Fix word normalization in text cleanup for gensim and sklearn

--- topic_model/custom_preprocessing.py
+++ topic_model/custom_preprocessing.py
@@ -25,7 +25,7 @@ class TextPreprocessor:
         text = self.hashtag_pattern.sub('', text)
         return text
 
-    def cleanup(self, text):
+    def remove_artefacts(self, text):
         """Text cleaning function inspired by the cleanup utility function in fastai.text.transform:
         https://github.com/fastai/fastai/blob/2c5eb1e219f55773330d6aa1649f3841e39603ff/fastai/text/transform.py#L58
         """
@@ -37,19 +37,47 @@ class TextPreprocessor:
               '“', '').replace('”', '').replace('’', '').replace('•', '').replace('—', '')
         return re1.sub(' ', html.unescape(text))
 
+    def normalize(self, text):
+        "Remove symbols/numbers and keep just words greater than a length of 2"
+        pattern = re.compile(r'(\W)')
+        content = pattern.sub(' ', text)  # Remove all symbols
+        # Remove pure digits (keep alphabets and word length >= 2) 
+        normalized_text = ' '.join(word for word in content.split() if word.isalpha() and len(word) > 1)
+        print(content)
+        return normalized_text
+
     def tokenize(self, text):
         "Tokenize input string using a spaCy pipeline"
         doc = self.sentencizer(text)
         tokenized_text = ' '.join(token.text for token in doc)
         return tokenized_text
 
-    def lemmatize(self, text, add_removed_words):
+    def lemmatize(self, text, add_removed_words=None):
         "Lemmatize text using a spaCy pipeline"
         stopwords = self.sentencizer.Defaults.stop_words
-        combined_stopwords = stopwords.union(add_removed_words)
+        if add_removed_words:
+            stopwords = stopwords.union(add_removed_words)
+        # Perform cleanup tasks
         text = self.clean_patterns(text)
-        text = self.cleanup(text)
+        text = self.remove_artefacts(text)
+        text = self.normalize(text)
+        # text = self.cleanup(text)
         doc = self.sentencizer(text)
         lemmas = [str(tok.lemma_).lower() for tok in doc
-                  if tok.text.lower() not in combined_stopwords]
+                  if tok.text.lower() not in stopwords]
         return lemmas
+
+
+# Test cleanup functiosn on an example
+if __name__ == "__main__":
+    text = "This is an example@abc.com email address. I've taken 10,000 items. <br />  \
+            I also have a phone number: 301-376-5784."
+    proc = TextPreprocessor()
+    text = proc.clean_patterns(text)
+    print(text)
+    text = proc.remove_artefacts(text)
+    print(text)
+    text = proc.normalize(text)
+    print(text)
+    text = proc.lemmatize(text)
+    print(text)
