52008e1cf229514bb017a4bb55b0b6d6cacf0278
prrao87 <prrao87@gmail.com>
2020-10-30 11:44:36 -0400

    Update stopwords and EDA notebook

--- src/stopwords/create_stopword_list.py
+++ src/stopwords/create_stopword_list.py
@@ -41,33 +41,36 @@ if __name__ == "__main__":
     spacy_lemmas = get_spacy_lemmas()
 
     # Create custom lists depending on the class of words seen in the data
-    person_titles = ['mr', 'mrs', 'ms', 'dr', 'mr.', 'mrs.', 'ms.', 'dr.']
+    person_titles = ['mr', 'mrs', 'ms', 'dr', 'mr.', 'mrs.', 'ms.', 'dr.', 'e']
     broken_words = ['don', 'isn', 'mustn', 'shouldn', 'couldn', 'doesn', 'didn']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '000']
-    url_terms = ['http', 'https', 'ref', 'href', 'com']
+    url_terms = ['http', 'https', 'ref', 'href', 'com', 'src']
     days_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday',
                         'saturday', 'sunday']
+    months_of_the_year = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
+                          'august', 'september', 'october', 'november', 'december']
     time_periods = ['minute', 'minutes', 'hour', 'hours', 'day', 'days', 'week', 'weeks',
                     'month', 'months', 'year', 'years']
     time_related = ['yesterday', 'today', 'tomorrow', 'day', 'night', 'morning',
                     'afternoon', 'evening', 'edt', 'est', 'time', 'times']
-    common_nouns = ['people', 'family', 'friend', 'father', 'mother', 'brother', 'sister',
-                    'son', 'daughter', 'life', 'man', 'woman', 'way', 'thing',
-                    'new', 'york', 'news']
+    common_nouns = ['press', 'news', 'report', 'page', 'user', 'file', 'video', 'pic',
+                    'photo', 'online', 'social', 'media', 'group', 'inbox', 'item',
+                    'advertisement', 'world', 'store', 'story', 'life', 'family',
+                    'people', 'man', 'woman', 'friend', 'friends']
     social_media = ['twitter', 'facebook', 'google', 'gmail', 'video', 'photo', 'image',
-                    'user', 'social', 'media', 'page', 'online', 'live', 'stream', 'post',
+                    'user', 'social', 'media', 'page', 'online', 'stream', 'post',
                     'app']
-    light_verb_roots = ['like', 'love', 'think', 'want', 'know', 'feel', 'look',
-                        'come', 'try', 'go', 'take', 'give', 'get', 'tell', 'ask', 'talk',
-                        'write', 'speak', 'say', 'comment', 'include', 'use', 'see', 'call',
-                        'make', 'find']
+    light_verb_roots = ['call', 'click', 'continue', 'comment', 'do', 'feel', 'find',
+                        'give', 'get', 'have', 'include', 'like', 'live', 'love', 'make',
+                        'post', 'read', 'say', 'speak', 'send', 'share', 'show', 'sign',
+                        'tag', 'take', 'tell', 'think', 'update', 'work', 'write']
     # Convert light verb roots to all its forms using lemma lookup
     light_verbs_full = lookup_verbs(light_verb_roots, spacy_lemmas)
 
     # Combine into a single lit of stopwords
     add_stopwords = set(
         combine(
-            person_titles, broken_words, numbers, url_terms, days_of_the_week, 
+            person_titles, broken_words, numbers, url_terms, days_of_the_week, months_of_the_year,
             time_periods, time_related, common_nouns, social_media, light_verbs_full
         )
     )
